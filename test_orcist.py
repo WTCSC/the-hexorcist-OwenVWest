eae1c3a80ba0607749c0d65ee397ee1603feff9b
OwenVWest <owenvanwestenberg@gmail.com>
2025-10-24 13:59:34 -0600

    updated tests

--- test_orcist.py
+++ test_orcist.py
@@ -1,16 +1,18 @@
-from HexingSpells import to_decimal, type_out, to_base
+from HexingSpells import to_decimal, to_base
 import pytest
 
 def test_to_decimal_conversion():
+    """Tests the conversions from provided base to base 10"""
     assert to_decimal("C7", 16) == 199
     assert to_decimal("1111000", 2) == 120
     assert to_decimal("4S", 36) == 172
     assert to_decimal("A", 18) == 10
     assert to_decimal("125", 6) == 53
 def test_to_base_conversion():
+    """Tests the conversions from base 10 to the provided base"""
     assert to_base(199, 16) == "C7"
-    assert to_base(48, 20) == "24"
+    assert to_base(48, 20) == "28"
     assert to_base(32, 3) == "1012"
-    assert to_base(182392, 33) == "52F4"
+    assert to_base(182392, 33) == "52G1"
     assert to_base(1, 1) == "1"
 
